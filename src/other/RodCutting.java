38f6fc392835578d7f916d4dac776f1eebdde73d
Rajat Pawar <rajatpawar@hotmail.com>
2016-03-21 13:24:20 -0400

    add rod cutting with dynamic; some minor changes

--- src/other/RodCutting.java
+++ src/other/RodCutting.java
@@ -42,7 +42,8 @@ public class RodCutting {
         int lastCut;
         char[] maxArray = null;
 
-        System.out.println("Create binary numbers and we make a cut at i+1 th index if index i is set in the string. ");
+        System.out
+                .println("Create binary numbers and we make a cut at i+1 th index if index i is set in the string. ");
         for (int i = 0; i < Math.pow(2, ((rodSize - 1))); i++) {
 
             lastCut = 0;
@@ -50,55 +51,59 @@ public class RodCutting {
 
             // Iterator over the number's binary representation to compute the
             // current cost.
-            
-            // add 1 cause the last end of rod is "always a cut". 
+
+            // add 1 cause the last end of rod is "always a cut".
             String binary = Integer.toBinaryString(i);
             StringBuffer myBuffer = new StringBuffer();
-            myBuffer.append(binary);myBuffer.append("1");
-            binary=myBuffer.toString();
+            myBuffer.append(binary);
+            myBuffer.append("1");
+            binary = myBuffer.toString();
 
             int expectedSize = rodSize;
             if (binary.length() < expectedSize) {
                 binary = padding(expectedSize - binary.length(), binary);
             }
-            
-            
+
             char[] charArray = binary.toCharArray();
-            
+
             for (int j = 0; j < charArray.length; j++) {
 
                 if (charArray[j] == '1') {
-                    int cutsize = j - lastCut +1;
-                    
-                    /* FOR DEBUG
-                    System.out.println(" cut at " + j + " and size is " + cutsize);
-                    System.out.println("Add cost " + priceArray[cutsize-1]);
-                    */
-                    
-                    currentPrice = currentPrice + priceArray[cutsize-1];
-                    lastCut = j+1;
+                    int cutsize = j - lastCut + 1;
+
+                    /*
+                     * FOR DEBUG System.out.println(" cut at " + j +
+                     * " and size is " + cutsize);
+                     * System.out.println("Add cost " + priceArray[cutsize-1]);
+                     */
+
+                    currentPrice = currentPrice + priceArray[cutsize - 1];
+                    lastCut = j + 1;
                 }
             }
             if (currentPrice > maxPrice) {
                 maxPrice = currentPrice;
                 maxArray = charArray;
             }
-            if (charArray != null)
-                System.out.println("Price for " + new String(charArray) + " is " + currentPrice);
-            
+
+            /*
+             * DEBUG if (charArray != null) System.out.println("Price for " +
+             * new String(charArray) + " is " + currentPrice);
+             */
+
             currentPrice = 0;
-            
+
         }
 
         HashSet<Integer> set = new HashSet<Integer>();
-        
+
         System.out.println("------------");
         System.out.println("For max price, We make the cut at following positions in the rod: ");
         if (maxArray != null) {
-            for (int k = 0; k < maxArray.length-1; k++) {
+            for (int k = 0; k < maxArray.length - 1; k++) {
                 if (maxArray[k] == '1')
-                    System.out.println(k+1);
-                    set.add(new Integer(k + 1));
+                    System.out.println(k + 1);
+                set.add(new Integer(k + 1));
 
             }
             System.out.println("------------");
@@ -107,9 +112,77 @@ public class RodCutting {
             System.out.println("Input not proper! ");
             return null;
         }
-        
+
     }
 
+    /**
+     * Solves problem using Dynamic Programming. Returns the maximum price (and
+     * not the cut spots).
+     * 
+     * @param rodSize
+     * @param priceArray
+     * @return Maximum Price
+     */
+    public int rodCuttingUsingDynamicProgramming(int rodSize, int[] priceArray) {
+
+        // create memoization array
+
+        int[] memoization = new int[rodSize];
+        memoization[0] = priceArray[0];
+
+        for (int i = 1; i < rodSize; i++) {
+
+            /*
+             * DEBUG System.out.println(
+             * "Compute the max price we can get for a piece of size " + (i+1));
+             */
+
+            // iterate over the length of the memoization array.
+
+            // for any i, we want to find the max price that we can get.
+            // now, for any i, we can put a cut at 0, 1, 2,3 .... i-1
+
+            // for wach of them , the cost would be p[i],
+            // p[i-1]+p[1],.....p[i]+p[i-1].
+            // we assign the max cost after complete iteration in the
+            // memoization array.
+            int endpoint = (i / 2);
+
+            // let us already assign the price as the one without any cuts
+            int maxPrice = priceArray[i];
+
+            for (int j = 0; j < i; j++) {
+
+                /*
+                 * DEBUG System.out.println("For a piece of size " + (i+1) +
+                 * " , let us consider pieces of size :  " + (j+1) + " and " +
+                 * ((i-j-1)+1));
+                 * System.out.println("Max price of a piece of size " + (j+1) +
+                 * " is " + memoization[j]);
+                 * System.out.println("Max price of a piece of size " +
+                 * ((i-j-1)+1) + " is " + memoization[i-j-1]);
+                 */
+
+                if ((memoization[j] + memoization[i - j - 1]) > maxPrice) {
+                    // DEBUG: System.out.println("Assign max " + maxPrice);
+                    maxPrice = (memoization[j] + memoization[i - j - 1]);
+                }
+
+            }
+            memoization[i] = maxPrice;
+
+        }
+
+        return memoization[rodSize - 1];
+    }
+
+    /**
+     * Add some zeroes before a string.
+     * 
+     * @param numOfZeros
+     * @param endString
+     * @return
+     */
     public String padding(int numOfZeros, String endString) {
         StringBuffer buffer = new StringBuffer();
         for (int b = 0; b < (numOfZeros); b++) {
--- test/other/RodCuttingTest.java
+++ test/other/RodCuttingTest.java
@@ -1,8 +1,5 @@
 package other;
 
-
-
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
@@ -11,41 +8,45 @@ import java.util.HashSet;
 import org.junit.Test;
 
 public class RodCuttingTest {
-    
-    
+
     @Test
     public void rodCuttingShouldWorkOnTrivialTestCases() {
-        
+
         // size = 4
-            
-        // __ __ __ __ 
-        //  1  2 3 4
+
+        // __ __ __ __
+        // 1 2 3 4
         // 20,10,5,1
-        
+
         // 0 to 7
         // 0 to 111
         // totalsize = rodSize-1
-        
+
         int[] priceArray = new int[4];
-        priceArray[0]=20;
-        priceArray[1]=500;
-        priceArray[2]=100;
-        priceArray[3]=1;
-        
+        priceArray[0] = 20;
+        priceArray[1] = 10;
+        priceArray[2] = 5;
+        priceArray[3] = 1;
+
+        int expectedPrice = 80;
+
         RodCutting rodCuttingSolver = new RodCutting();
-        
+
         HashSet<Integer> expected = new HashSet<Integer>();
-        expected.add(1);expected.add(2);expected.add(3);
-        assertEquals("Rod cutting failed! ",expected,rodCuttingSolver.getCutPositionsUsingBruteForce(priceArray.length, priceArray));
-         
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+        assertEquals("Rod cutting with brute force failed!  ", expected,
+                rodCuttingSolver.getCutPositionsUsingBruteForce(priceArray.length, priceArray));
+        assertEquals("Rod cutting with dynamic programming failed!  ", expectedPrice,
+                rodCuttingSolver.rodCuttingUsingDynamicProgramming(priceArray.length, priceArray));
+
     }
-    
-    
+
     @Test
     public void paddingShouldWork() {
         RodCutting r = new RodCutting();
-        System.out.println(r.padding(3, "10"));
-        assertEquals("","00010",r.padding(3, "10"));
+        assertEquals("", "00010", r.padding(3, "10"));
     }
 
 }
